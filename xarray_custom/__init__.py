58c395171f0c542dd905331ba4f38508ad1e05c7
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-05-09 21:53:09 +0900

    Update __init__ module

--- xarray_custom/__init__.py
+++ xarray_custom/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "0.1.0"
+# flake8: noqa
+__version__ = "0.2.0"
+__author__ = "Akio Taniguchi"
+
+
+# modules
+from . import dataclasses
+from . import typing
+
+
+# aliases
+from .dataclasses import *
