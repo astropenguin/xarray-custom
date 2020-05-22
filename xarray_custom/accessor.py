3f6631eedf7d5de694dd76663eec5522030ee5fa
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-05-22 18:48:39 +0900

    #5 Update accessor module

--- xarray_custom/accessor.py
+++ xarray_custom/accessor.py
@@ -2,28 +2,30 @@ __all__ = ["add_methods_to_accessor"]
 
 
 # standard library
+import re
 from functools import wraps
-from typing import Optional
+from typing import Any
 
 
 # dependencies
 from xarray import register_dataarray_accessor
 
 
+# constants
+SPECIAL_METHOD = "^__.+__$"
+
+
 # main functions
-def add_methods_to_accessor(cls: type, accessor: Optional[str] = None) -> type:
+def add_methods_to_accessor(cls: type) -> type:
     """Create a DataArray accessor and add methods in a class to it.
 
     Args:
         cls: Custom DataArray class.
-        accessor: Name of a DataArray accessor.
 
     Returns:
         cls: Same as ``cls`` in the arguments.
 
     """
-    if accessor is None:
-        return cls
 
     class Accessor:
         def __init__(self, _accessed):
@@ -37,13 +39,19 @@ def add_methods_to_accessor(cls: type, accessor: Optional[str] = None) -> type:
         return accessor_method
 
     for name, obj in cls.__dict__.items():
-        if not callable(obj):
-            continue
+        if is_user_defined_method(obj):
+            setattr(Accessor, name, convert(obj))
 
-        if name.startswith("__"):
-            continue
-
-        setattr(Accessor, name, convert(obj))
-
-    register_dataarray_accessor(accessor)(Accessor)
+    register_dataarray_accessor(cls.accessor)(Accessor)
     return cls
+
+
+def is_user_defined_method(obj: Any) -> bool:
+    """Return whether ``obj`` is a user-defined method."""
+    if not callable(obj):
+        return False
+
+    if re.search(SPECIAL_METHOD, obj.__name__):
+        return False
+
+    return True
