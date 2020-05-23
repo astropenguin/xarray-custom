038595ed01c792952084d308d5c239d64d56373d
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-05-23 18:26:11 +0900

    #5 Update module docstring

--- xarray_custom/dataclasses.py
+++ xarray_custom/dataclasses.py
@@ -6,6 +6,59 @@ with fixed dimensions, datatype, and coordinates. Two functions are available:
 - ``dataarrayclass``: Class decorator which updates a custom DataArray class.
 - ``ctype``: Create a custom DataArray class for the definition of a coordinate.
 
+Examples:
+    To create a custom DataArray class to represent images::
+        @dataarrayclass(accessor='img')
+        class Image:
+            dims = 'x', 'y'
+            dtype = float
+            x: ctype('x', int) = 0
+            y: ctype('y', int) = 0
+            w: ctype(('x', 'y'), float) = 1.0
+
+            def normalize(self):
+                return self / self.max()
+
+    The code style is similar to that of Python's dataclass.
+    A DataArray is then created using the class::
+        image = Image([[0, 1], [2, 3]], x=[0, 1], y=[0, 1])
+        print(image)
+
+        # <xarray.DataArray (x: 2, y: 2)>
+        # array([[0., 1.],
+        #        [2., 3.]])
+        # Coordinates:
+        #   * x        (x) int64 0 1
+        #   * y        (y) int64 0 1
+        #     w        (x, y) float64 1.0 1.0 1.0 1.0
+
+    Because ``dims``, ``dtype``, and coordinates are pre-defined,
+    it is much easier to create a DataArray with given data.
+    Custom methods can be used via an accessor::
+        normalized = image.img.normalize()
+        print(normalized)
+
+        # <xarray.DataArray (x: 2, y: 2)>
+        # array([[0.        , 0.33333333],
+        #        [0.66666667, 1.        ]])
+        # Coordinates:
+        #   * x        (x) int64 0 1
+        #   * y        (y) int64 0 1
+        #     w        (x, y) float64 1.0 1.0 1.0 1.0
+
+    Like NumPy, several special class methods are available
+    to create a DataArray filled with some values::
+        ones = Image.ones((2, 2))
+        print(ones)
+
+        # <xarray.DataArray (x: 2, y: 2)>
+        # array([[1., 1.],
+                 [1., 1.]])
+        # Coordinates:
+        #   * x        (x) int64 0 0
+        #   * y        (y) int64 0 0
+        #     w        (x, y) float64 1.0 1.0 1.0 1.0
+
 """
 __all__ = ["ctype", "dataarrayclass"]
 
