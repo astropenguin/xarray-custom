c69e64f7ffba9be1f995705af7521db71f716b0f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-06-07 16:52:25 +0900

    #13 Update docstring module

--- xarray_custom/docstring.py
+++ xarray_custom/docstring.py
@@ -1,101 +1,107 @@
 """Module for formatting docstrings of custom DataArray classes.
 
-This module provides a function (``format_doc``) which formats
-docstrings of special methods in a custom DataArray class.
+This module provides the following functions which are used in
+the metaclass (``DataArrayClassMeta``) of custom DataArray classes.
+
+- ensure_desc: Make sure that a class has a valid ``desc`` attribute.
+- update_doc: Copy a function and update docstring according to a class attributes.
 
 """
-__all__ = ["format_doc"]
+__all__ = ["ensure_desc", "update_doc"]
 
 
-# standard library
+import re
 from functools import wraps
-from textwrap import dedent, indent, TextWrapper
-from typing import Callable
+from textwrap import dedent, TextWrapper
+from typing import Callable, List
 
 
 # constants
-HALF_INDENT = " " * 2
-INDENT = " " * 4
 WIDTH = 80
+INDENT = " " * 4
 
 
 # main functions
-def format_doc(func: Callable, cls: type) -> Callable:
-    """Copy a function and format its docstring according to a class attributes.
+def ensure_desc(cls):
+    """Make sure that a class has a valid ``desc`` attribute.
 
-    Args:
-        func: Function to be copied and formatted.
-        cls: Custom DataArray class whose attributes are used for formatting.
-
-    Returns:
-        func: Copied function whose docstring is updated.
+    If it is None, then metaclass ``desc`` is set.
+    Line breaks and indents are replaced with whitespaces.
 
     """
-    summary = create_summary(cls)
-    coords_doc = create_coords_doc(cls)
-
-    doc = dedent(func.__doc__)
-    doc = doc.format(summary=summary, coords_doc=coords_doc)
-
-    func = copy(func)
-    func.__doc__ = doc
-    return func
+    if cls.desc is None:
+        cls.desc = type(cls).desc
+    else:
+        cls.desc = re.sub(r"\n\s*", " ", cls.desc)
 
 
-# helper functions
-def copy(func: Callable) -> Callable:
-    """Copy a function as a decorated one."""
+def update_doc(func: Callable, cls: type) -> Callable:
+    """Copy a function and update docstring according to a class attributes.
+
+    Args:
+        func: Function to be copied and updated.
+        cls: Custom DataArray class whose attributes are used for updating.
+
+    Returns:
+        Copied function whose docstring is updated.
+
+    """
+    formatter = dict(
+        coords_args=wrap(create_coords_args(cls), INDENT, INDENT * 2),
+        summary=wrap(create_summary(cls), "- ", "  "),
+    )
 
     @wraps(func)
     def copied(*args, **kwargs):
         return func(*args, **kwargs)
 
+    copied.__doc__ = dedent(copied.__doc__).format(**formatter)
     return copied
 
 
-def create_summary(cls: type) -> str:
-    """Create a docstring to summarize a class."""
-    coords = [f"``{name}``" for name in cls.ctypes]
+# helper functions
+def create_summary(cls: type) -> List[str]:
+    """Create docstrings to summarize a class."""
+    dims = str(cls.dims).replace("'", "")
 
-    docs = [
-        f"- **desc:** {cls.desc}",
-        f"- **dims:** ``{cls.dims!r}``",
-        f"- **dtype:** ``{cls.dtype!r}``",
-        f"- **coords:** {', '.join(coords)}",
-    ]
+    try:
+        dtype = cls.dtype.__name__
+    except AttributeError:
+        dtype = cls.dtype
 
-    return "\n".join(indent_wrap(doc, False) for doc in docs)
-
-
-def create_coords_doc(cls: type) -> str:
-    """Create a docstring of coordinates of a class."""
-    docs = []
-
-    for name, ctype in cls.ctypes.items():
-        docs.append(
-            indent_wrap(
-                f"{name}: "
-                f"(dims: ``{ctype.dims!r}``, "
-                f"dtype: ``{ctype.dtype!r}``) "
-                f"{ctype.desc}"
-            ),
-        )
-
-    return "\n".join(indent(doc, INDENT) for doc in docs)
+    yield f"desc: {cls.desc}"
+    yield f"dims: {dims}"
+    yield f"dtype: {dtype}"
+    yield f"accessor: {cls.accessor}"
+    yield f"coords: {', '.join(cls.coords)}"
 
 
-def indent_wrap(doc: str, inside_section: bool = True) -> str:
-    """Wrap a docstring with leading indents from the second line.
+def create_coords_args(cls: type) -> List[str]:
+    """Create docstrings of coordinates of a class."""
+    for name, ctype in cls.coords.items():
+        dims = str(ctype.dims).replace("'", "")
+
+        try:
+            dtype = ctype.dtype.__name__
+        except AttributeError:
+            dtype = ctype.dtype
+
+        yield f"{name}: (dims={dims}, dtype={dtype}) {ctype.desc}"
+
+
+def wrap(docs: List[str], initial_indent: str, subsequent_indent: str) -> str:
+    """Wrap and join docstrings with indents.
 
     Args:
-        doc: String to be wrapped.
-        inside_section: Whether the wrapped ``doc`` is used inside
-            a section (e.g., Args or Returns). If False (outside),
-            half indent (two whitespaces) is alternatively used.
+        docs: Docstrings to be wrapped and joined.
+        initial_indent: String to be prepended to the first line
+            of each wrapped docstring.
+        subsequent_indent: String to be prepended to the second,
+            third, ... lines of each wrapped docstring.
 
     Returns:
-        wrapped: Wrapped string.
+        Wrapped and joined string.
 
     """
-    indent = INDENT if inside_section else HALF_INDENT
-    return "\n".join(TextWrapper(WIDTH, "", indent).wrap(doc))
+    wrapper = TextWrapper(WIDTH, initial_indent, subsequent_indent)
+    return "\n".join("\n".join(wrapper.wrap(doc)) for doc in docs)
