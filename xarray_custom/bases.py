35fd92c419d8835ce9d4443e02f99538d671c895
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-06-14 18:22:06 +0900

    #13 Update DataArray's base class and its metaclass

--- xarray_custom/bases.py
+++ xarray_custom/bases.py
@@ -2,105 +2,98 @@ __all__ = ["DataArrayClass"]
 
 
 # standard library
-from typing import Callable, Dict, List, Optional, Union
+from functools import wraps
+from typing import Callable, Dict, Optional, Union
 from typing import get_type_hints
 
 
 # dependencies
+from .docstring import update_doc
 from .special import empty, zeros, ones, full, new
 from .typing import Attrs, Dims, Dtype, Name
 
 
-# main classes
+# helper classes
+class classproperty(property):
+    pass
+
+
 class DataArrayClassMeta(type):
+    def __new__(meta, name, bases, attrs):
+        for key, val in attrs.copy().items():
+            if isinstance(val, classmethod):
+                setattr(meta, key, val.__func__)
+                attrs.pop(key)
+
+            if isinstance(val, classproperty):
+                setattr(meta, key, val)
+                attrs.pop(key)
+
+        return super().__new__(meta, name, bases, attrs)
+
+
+# main classes
+class DataArrayClass(metaclass=DataArrayClassMeta):
     # class attributes for DataArray
     attrs: Optional[Attrs] = None
     dims: Optional[Dims] = None
     dtype: Optional[Dtype] = None
     name: Optional[Name] = None
-
-    # class attributes for options
+    desc: Optional[str] = None
     accessor: Optional[str] = None
-    desc: str = "No description."
 
-    def __new__(meta, name, bases, attrs) -> "DataArrayClassMeta":
-        attrs["desc"] = attrs.get("__doc__", meta.desc)
-        return super().__new__(meta, name, bases, attrs)
+    @wraps(new)
+    def __new__(cls, *args, **kwargs):
+        return new(cls, *args, **kwargs)
 
-    def __init__(cls, name, bases, attrs) -> None:
-        def __new__(cls, *args, **kwargs):
-            return cls.new(*args, **kwargs)
+    @classproperty
+    def new(cls) -> Callable:
+        return update_doc(new, cls).__get__(cls)
 
-        cls.__new__ = __new__
+    @classproperty
+    def empty(cls) -> Callable:
+        return update_doc(empty, cls).__get__(cls)
 
-    @property
+    @classproperty
+    def zeros(cls) -> Callable:
+        return update_doc(zeros, cls).__get__(cls)
+
+    @classproperty
+    def ones(cls) -> Callable:
+        return update_doc(ones, cls).__get__(cls)
+
+    @classproperty
+    def full(cls) -> Callable:
+        return update_doc(full, cls).__get__(cls)
+
+    @classproperty
     def data(cls) -> Dict[str, Union[Dims, Dtype]]:
         """Properties of multi-dimensional data."""
         return dict(dims=cls.dims, dtype=cls.dtype)
 
-    @property
-    def coords(cls) -> Dict[str, "DataArrayClassMeta"]:
+    @classproperty
+    def coords(cls) -> Dict[str, type]:
         """Properties of coordinates of data."""
         coords = {}
 
         for name, hint in get_type_hints(cls).items():
-            if isinstance(hint, DataArrayClassMeta):
+            if not isinstance(hint, type):
+                continue
+
+            if issubclass(hint, DataArrayClass):
                 coords[name] = hint
 
         return coords
 
-    @property
-    def new(cls) -> Callable:
-        cls._new = classmethod(new)
-        return cls._new
-
-    @property
-    def empty(cls) -> Callable:
-        cls._empty = classmethod(empty)
-        return cls._empty
-
-    @property
-    def zeros(cls) -> Callable:
-        cls._zeros = classmethod(zeros)
-        return cls._zeros
-
-    @property
-    def ones(cls) -> Callable:
-        cls._ones = classmethod(ones)
-        return cls._ones
-
-    @property
-    def full(cls) -> Callable:
-        cls._full = classmethod(full)
-        return cls._full
-
-    @property
+    @classproperty
     def __doc__(cls) -> str:
-        return cls.new.__doc__
-
-    def __repr__(cls) -> str:
-        try:
-            return cls.__class_repr__()
-        except AttributeError:
-            return super().__repr__()
-
-    def __dir__(cls) -> List[str]:
-        dir_cls = super().__dir__()
-        dir_meta = dir(type(cls))
-
-        return list(set(dir_cls) | set(dir_meta))
-
-
-class DataArrayClass(metaclass=DataArrayClassMeta):
-    """Base of custom DataArray class."""
+        """Updatable class docstring."""
+        return update_doc(new, cls).__doc__
 
     @classmethod
-    def __class_repr__(cls):
+    def __repr__(cls) -> str:
+        """Updatable class repr string."""
         dims = str(cls.dims).replace("'", "")
-
-        try:
-            dtype = cls.dtype.__name__
-        except AttributeError:
-            dtype = cls.dtype
+        dtype = str(cls.dtype).replace("'", "")
 
         return f"{cls.__name__}(dims={dims}, dtype={dtype})"
