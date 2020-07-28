4ac93c6f4825288e8f7f49613c9a6a17b82a0eb5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-07-28 17:48:04 +0900

    #13 Use under names instead of dunder ones

--- xarray_custom/accessor.py
+++ xarray_custom/accessor.py
@@ -28,35 +28,35 @@ class DataArrayAccessorBase:
 
     def __init__(self, dataarray: DataArray):
         """Initialize an instance with a DataArray to be accessed."""
-        self.__dataarray = dataarray
+        self._dataarray = dataarray
 
     @lru_cache(None)
-    def __bind_function(self, func: Callable) -> Callable:
+    def _bind_function(self, func: Callable) -> Callable:
         """Bind a function to an instance to use it as a method."""
         first_arg = list(signature(func).parameters)[0]
 
         pattern = rf"(?<!\w){first_arg}\."
-        repl = rf"{first_arg}.{self.__name}."
+        repl = rf"{first_arg}.{self._name}."
         source = dedent(getsource(func))
 
         exec(sub(pattern, repl, source), func.__globals__, locals())
-        return locals()[func.__name__].__get__(self.__dataarray)
+        return locals()[func.__name__].__get__(self._dataarray)
 
     def __dir__(self) -> List[str]:
         """List names in the namespace of the DataArray class."""
-        return dir(self.__dataarrayclass)
+        return dir(self._dataarrayclass)
 
     def __getattr__(self, name: str) -> Any:
         """Get a bound method or an attribute of the DataArray class."""
         try:
-            return getattr(self.__dataarray, name)
+            return getattr(self._dataarray, name)
         except AttributeError:
-            obj = getattr(self.__dataarrayclass, name)
+            obj = getattr(self._dataarrayclass, name)
 
         if isinstance(obj, FunctionType):
-            return self.__bind_function(obj)
+            return self._bind_function(obj)
 
         if isinstance(obj, property):
-            return self.__bind_function(obj.fget)
+            return self._bind_function(obj.fget)
 
         return obj
