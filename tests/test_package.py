b287c8ed751bb49310850dce60b5340f9b006325
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-04 15:22:30 +0900

    #16 Add test_author function

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,7 +1,11 @@
 # dependencies
-from xarray_custom import __version__
+from xarray_custom import __author__, __version__
 
 
 # test functions
 def test_version():
     assert __version__ == "0.6.0"
+
+
+def test_author():
+    assert __author__ == "Akio Taniguchi"
